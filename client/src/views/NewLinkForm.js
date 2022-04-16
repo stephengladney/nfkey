c359309184f7b6d6cc3564fb5022275946f33a28
Stephen Gladney <stephengladney@gmail.com>
2022-04-15 22:53:51 -0400

    Format NewLinkForm css for mobile

--- client/src/views/NewLinkForm.js
+++ client/src/views/NewLinkForm.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { Fragment, useEffect, useRef, useState } from "react"
 import styled from "styled-components"
 import { generateId } from "../lib/id"
 import { getLink } from "../lib/api"
@@ -39,7 +39,7 @@ const Highlighted = styled.span`
 const BodyContainer = styled.div`
   margin-top: 30px;
   @media screen and (max-width: 768px) {
-    max-width: 75%;
+    width: 80%;
   }
 
   @media screen and (min-width: 769px) and (max-width: 1024px) {
@@ -64,10 +64,16 @@ const StyledInput = styled.input`
   font-family: "Mukta";
   font-size: 1em;
   padding: 6px;
-  width: 100%;
+
+  @media screen and (min-width: 1025px) {
+    width: 100%;
+  }
 `
+
 const InputErrorText = styled.span`
   color: #d00;
+  font-family: "Mukta";
+  font-size: 14px;
 `
 
 const StyledLabel = styled.label`
@@ -80,26 +86,63 @@ const StyledLabel = styled.label`
 `
 
 const FormContainer = styled.div`
-  /* text-align: center; */
   margin-top: 40px;
 `
 
 const FormLeftColumn = styled.div`
   display: flex;
-  justify-content: flex-end;
+  /* justify-content: flex-end; */
   align-items: center;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+  }
+
+  @media screen and (min-width: 769px) and (max-width: 1024px) {
+    justify-content: flex-end;
+  }
+
+  @media screen and (min-width: 1025px) {
+    justify-content: flex-end;
+  }
 `
 
 const FormRightColumn = styled.div`
   display: flex;
-  grid-column-start: 2;
+
   /* text-align: left; */
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    /* grid-row-start: 2; */
+  }
+
+  @media screen and (min-width: 769px) and (max-width: 1024px) {
+    grid-column-start: 2;
+  }
+
+  @media screen and (min-width: 1025px) {
+    justify-content: flex-end;
+    grid-column-start: 2;
+  }
 `
 
 const FormRow = styled.div`
   display: grid;
-  grid-template-columns: 1fr 2fr;
-  margin-bottom: 20px;
+  @media screen and (max-width: 768px) {
+    grid-template-rows: 1fr 1fr;
+    /* margin-bottom: 10px; */
+  }
+
+  @media screen and (min-width: 769px) and (max-width: 1024px) {
+    grid-template-columns: 1fr 2fr;
+  }
+
+  @media screen and (min-width: 1025px) {
+    grid-template-columns: 1fr 2fr;
+    margin-bottom: 20px;
+  }
 `
 
 const StyledButton = styled.button`
@@ -110,9 +153,30 @@ const StyledButton = styled.button`
   font-family: "Mukta";
   font-size: 1em;
   font-weight: 700;
-  margin-left: 10px;
+
   padding: 7px;
   text-transform: uppercase;
+
+  @media screen and (max-width: 768px) {
+    flex-grow: 1;
+    margin-top: 10px;
+    /* width: 100%; */
+  }
+  @media screen and (min-width: 1025px) {
+    flex-grow: 1;
+    margin-left: 10px;
+  }
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
+  margin-top: 34px;
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+  @media screen and (min-width: 1025px) {
+    flex-direction: row;
+  }
 `
 
 const NFKeyURL = styled.label`
@@ -121,6 +185,21 @@ const NFKeyURL = styled.label`
   font-size: 1em;
   font-weight: 700;
   margin-right: 15px;
+`
+
+const NFKeyURLSpacer = styled.div`
+  @media screen and (max-width: 768px) {
+    /* grid-template-rows: 1fr 1fr; */
+  }
+
+  @media screen and (min-width: 769px) and (max-width: 1024px) {
+    /* grid-template-columns: 1fr 2fr; */
+  }
+
+  @media screen and (min-width: 1025px) {
+    flex-grow: 1;
+    text-align: left;
+  }
 `
 
 export function NewLinkForm({}) {
@@ -141,22 +220,22 @@ export function NewLinkForm({}) {
     setSmartContractAddress(e.target.value)
   }
 
-  useEffect(() => {
-    clearTimeout(typingTimer.current)
-    typingTimer.current = setTimeout(() => {
-      getLink("localhost:3000", urlPath)
-        .then((response) => response.json())
-        .then((link) => {
-          console.log(urlPath)
-          if (link.url) {
-            setIsUrlPathTaken(true)
-          } else {
-            setIsUrlPathTaken(false)
-          }
-        })
-        .catch((e) => alert(`error getting link: ${e}`))
-    }, 1000)
-  }, [urlPath])
+  // useEffect(() => {
+  //   clearTimeout(typingTimer.current)
+  //   typingTimer.current = setTimeout(() => {
+  //     getLink("localhost:3000", urlPath)
+  //       .then((response) => response.json())
+  //       .then((link) => {
+  //         console.log(urlPath)
+  //         if (link.url) {
+  //           setIsUrlPathTaken(true)
+  //         } else {
+  //           setIsUrlPathTaken(false)
+  //         }
+  //       })
+  //       .catch((e) => alert(`error getting link: ${e}`))
+  //   }, 1000)
+  // }, [urlPath])
 
   return (
     <Container>
@@ -164,11 +243,11 @@ export function NewLinkForm({}) {
         NF<Highlighted>Key</Highlighted>
       </Title>
       <BodyContainer>
-        <BodyText>Please complete all of the fields below.</BodyText>
+        <BodyText>Please complete the fields below.</BodyText>
         <FormContainer>
           <FormRow>
             <FormLeftColumn>
-              <div style={{ flexGrow: 1, textAlign: "left" }}>
+              <div style={{}}>
                 <StyledLabel>Shortlink</StyledLabel>
               </div>
               <NFKeyURL>nfkey.to/</NFKeyURL>
@@ -178,14 +257,20 @@ export function NewLinkForm({}) {
               <StyledInput
                 isError={isUrlPathTaken}
                 onChange={handleUrlPathChange}
+                spellCheck={false}
                 value={urlPath}
               />
-
-              <StyledButton onClick={handleRandomizeClick}>
-                Randomize
-              </StyledButton>
             </FormRightColumn>
           </FormRow>
+
+          {isUrlPathTaken && (
+            <FormRow style={{ marginBottom: 10, marginTop: -20 }}>
+              <FormRightColumn>
+                <InputErrorText>URL is taken</InputErrorText>
+              </FormRightColumn>
+            </FormRow>
+          )}
+
           <FormRow>
             <FormLeftColumn>
               <StyledLabel>Smart contract address</StyledLabel>
@@ -193,17 +278,21 @@ export function NewLinkForm({}) {
             <FormRightColumn>
               <StyledInput
                 onChange={handleSmartContractAddressChange}
-                style={{ minWidth: "350px" }}
+                spellCheck={false}
               />
             </FormRightColumn>
           </FormRow>
-
-          <StyledButton
-            onClick={handleRandomizeClick}
-            style={{ marginTop: "40px" }}
-          >
-            Generate Link
-          </StyledButton>
+          <ButtonContainer>
+            <StyledButton onClick={handleRandomizeClick}>
+              Randomize URL
+            </StyledButton>
+            <StyledButton
+              onClick={handleRandomizeClick}
+              // style={{ marginTop: "40px" }}
+            >
+              Create Shortlink
+            </StyledButton>
+          </ButtonContainer>
         </FormContainer>
       </BodyContainer>
     </Container>
