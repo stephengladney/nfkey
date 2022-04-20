1fca68172c1d306a3f0d17c25693cd7dfec0b8be
Stephen Gladney <stephengladney@gmail.com>
2022-04-20 00:00:19 -0400

    set contentWindow.document.location

--- client/src/lib/iframe.js
+++ client/src/lib/iframe.js
@@ -1,20 +1,9 @@
 export function generateIframe(url) {
-  const blobMe = URL["createObjectURL"](new Blob([""], { type: "text/html" }))
   const elIframe = document["createElement"]("iframe")
   elIframe["setAttribute"]("id", "itme")
   elIframe["setAttribute"]("frameborder", "0")
   elIframe["setAttribute"]("width", "100%")
   elIframe["setAttribute"]("style", "height: 100vh")
-  elIframe["setAttribute"]("allowfullscreen", "true")
-  elIframe["setAttribute"]("webkitallowfullscreen", "true")
-  elIframe["setAttribute"]("mozallowfullscreen", "true")
-  elIframe["setAttribute"]("src", blobMe)
-  const idOne = "gepa_" + Date.now()
-  elIframe["setAttribute"]("id", idOne)
   document.getElementById("container").appendChild(elIframe)
-
-  const iframe = document.getElementById(idOne)
-  iframe.contentWindow.document.write(
-    '<script type="text/javascript">location.href = "' + url + '";\x3c/script>'
-  )
+  document.getElementById("itme").contentWindow.document.location = url
 }
--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
+      rejectUnauthorized: false,
     },
   },
 })
