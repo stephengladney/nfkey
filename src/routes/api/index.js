0d4e5025dff5d2517a073748574e36d9c0333d2e
Stephen Gladney <stephengladney@gmail.com>
2022-04-26 11:55:45 -0400

    Add back verifyRouter

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
 const linksRouter = require("./links")
+const verifyRouter = require("./verify")
 const visitRouter = require("./visits")
 
 router.use("/link", linksRouter)
