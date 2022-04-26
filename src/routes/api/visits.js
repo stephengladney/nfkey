edfc9a6745117f6fe0c6c1778ea692aac2220987
Stephen Gladney <stephengladney@gmail.com>
2022-04-26 11:58:30 -0400

    Log error

--- src/routes/api/visits.js
+++ src/routes/api/visits.js
@@ -4,7 +4,10 @@ const { Visit } = require("../../models")
 router.post("", (req, res) => {
   Visit.create(req.body)
     .then((_) => res.status(200).send())
-    .catch((_) => res.status(503).send())
+    .catch((e) => {
+      res.status(503).send()
+      console.log(e)
+    })
 })
 
 module.exports = router
