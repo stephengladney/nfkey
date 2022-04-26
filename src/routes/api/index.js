74229a2706a4923de70fc14067cf570d7219101b
Stephen Gladney <stephengladney@gmail.com>
2022-04-26 11:54:21 -0400

    fix visit path typo

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const linksRouter = require("./links")
-const visitRouter = require("./visit")
+const visitRouter = require("./visits")
 
 router.use("/link", linksRouter)
 router.use("/verify", verifyRouter)
