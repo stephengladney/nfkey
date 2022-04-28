7f672bf49e8eac4604c3f462a91abf590cf454c1
Stephen Gladney <stephengladney@gmail.com>
2022-04-28 19:41:14 -0400

    rm htaccess file

--- client/src/views/const.js
+++ client/src/views/const.js
@@ -22,6 +22,6 @@ export const URL_FEEDBACK = {
 export const REGEX = {
   URL_PATH: /^[a-zA-Z0-9-_]*$/,
   DESTINATION_URL:
-    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,18}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,
+    /(https:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,18}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,
   SMART_CONTRACT: /^0x[a-fA-F0-9]{40}$/,
 }
