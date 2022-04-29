c0be6849180e8d52dc79255ac39594aa7fb169e0
Stephen Gladney <stephengladney@gmail.com>
2022-04-29 00:24:40 -0400

    Add all the things

--- src/app.js
+++ src/app.js
@@ -4,23 +4,26 @@ const routes = require("./routes")
 const http = require("http")
 const https = require("https")
 const app = express()
-const httpServer = http.createServer(app)
-const httpsServer = https.createServer(app)
+const fs = require("fs")
 const { sequelize } = require("./config/sequelize")
 const path = require("path")
 const bodyParser = require("body-parser")
-const { moveMessagePortToContext } = require("worker_threads")
 
 sequelize.sync()
 
+const httpsOptions = {
+  cert: fs.readFileSync("../.ssl/nfkey_to.crt"),
+  ca: fs.readFileSync("../.ssl/nfkey_to.ca-bundle"),
+  key: fs.readFileSync("../.ssl/nfkey_to.p7b"),
+}
+
+const httpServer = http.createServer(app)
+const httpsServer = https.createServer(httpsOptions, app)
+
 app.use((req, res, next) => {
-  console.log(
-    "======================",
-    `PROTOCOL: ${req.protocol}`,
-    `HOST: ${req.headers.host}`,
-    `URL: ${req.url}`,
-    "======================"
-  )
+  if (req.protocol == "http") {
+    res.redirect(301, `https://${req.headers.host}${req.url}`)
+  }
   next()
 })
 
@@ -28,6 +31,8 @@ app.use(express.static(path.resolve("client", "build")))
 app.use(bodyParser.json())
 app.use(routes)
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("nfkey server is running!")
-})
+// app.listen(process.env.PORT || 5000, () => {
+//   console.log("nfkey server is running!")
+// })
+httpServer.listen(80, "nfkey.to")
+httpsServer.listen(443, "nfkey.to")
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,19 +12,6 @@ router.get("/googleaf69fafbcd3f09b4.html", (req, res) => {
   res.sendFile(path.resolve("googleaf69fafbcd3f09b4.html"))
 })
 
-router.get(
-  "/.well-known/pki-validation/E2ADFCB28576923E02E3C2786EDD3283.txt",
-  (req, res) => {
-    res.sendFile(
-      path.resolve(
-        ".well-known",
-        "pki-validation",
-        "E2ADFCB28576923E02E3C2786EDD3283.txt"
-      )
-    )
-  }
-)
-
 router.get("*", (req, res) => {
   res.sendFile(path.resolve("client", "build", "index.html"))
 })
