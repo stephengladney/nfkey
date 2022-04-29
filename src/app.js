052be3cfb204f6bf3c6ab3ad4a8f6a189d71f1a5
Stephen Gladney <stephengladney@gmail.com>
2022-04-29 00:56:13 -0400

    rm unnecessary stuff

--- src/app.js
+++ src/app.js
@@ -11,18 +11,9 @@ const bodyParser = require("body-parser")
 
 sequelize.sync()
 
-// const httpsOptions = {
-//   cert: fs.readFileSync("./.ssl/nfkey_to.crt"),
-//   ca: fs.readFileSync("./.ssl/nfkey_to.ca-bundle"),
-//   key: fs.readFileSync("./.ssl/example_com.key"),
-// }
-
-// const httpServer = http.createServer(app)
-// const httpsServer = https.createServer(httpsOptions, app)
-
 app.use((req, res, next) => {
   if (req.headers["x-forwarded-proto"] != "https") {
-    res.redirect("https://nfkey.to" + req.url)
+    res.redirect(req.headers.host + req.url)
   } else next()
 })
 
@@ -33,5 +24,3 @@ app.use(routes)
 app.listen(process.env.PORT || 5000, () => {
   console.log("nfkey server is running!")
 })
-// httpServer.listen(process.env.PORT || 80, "nfkey.to")
-// httpsServer.listen(process.env.PORT, "nfkey.to")
