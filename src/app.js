533ef4c72c0fd5b8b6efc9e17bb43cdc5b0ec4a8
Stephen Gladney <stephengladney@gmail.com>
2022-04-29 12:51:27 -0400

    make redirect 301

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ sequelize.sync()
 
 app.use((req, res, next) => {
   if (req.headers["x-forwarded-proto"] != "https") {
-    res.redirect(`https://${req.headers.host}${req.url}`)
+    res.redirect(301, `https://${req.headers.host}${req.url}`)
   } else next()
 })
 
